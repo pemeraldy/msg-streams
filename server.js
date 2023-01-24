212122e7726e86327d5697a977ad2965b696be28
Peter Olu <pemeraldy@gmail.com>
2023-01-24 03:54:14 +0100

    established db connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const  mongoose = require('mongoose');
 const app = require('./src/index')
 require('dotenv').config()
 
@@ -5,8 +6,22 @@ const config = {
   port: process.env.PORT || 3000,
 }
 
+let client
+const initializeDB = async ()=>{
+  client  =  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => console.log(err));
+}
+
+
 const server = app.listen(config.port, () => {
-  console.log("Server started on port!!!", config.port);
+  initializeDB();
+  console.log("Server started on port", config.port);
 });
+
+
 
 module.exports = server
